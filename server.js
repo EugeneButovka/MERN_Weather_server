5949e6d3850d8934ed77532f286a08ee0a0ec17d
EugeneButovka <eugene.butovka@gmail.com>
2019-07-18 15:14:40 +0300

    made register and login TODO: jwt and hash auth

--- server.js
+++ server.js
@@ -5,33 +5,23 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json()); // body parser for JSON middleware
 
-const db = require('./server/config/keys').mongoURI;
+const db = require('./config/keys').mongoURI;
 
 //connect ot mongoDB
 mongoose.connect(
     db,
     {
         useNewUrlParser: true,
-        useCreateIndex: true //to eliminate message: DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
+        useCreateIndex: true
     }
     )
     .then(() => console.log('mongoDB connected'))
     .catch(err => console.log(err));
 
 //use routes
-app.use('/auth', require('./server/routes/auth.route')); // POST /login  POST /register
-app.use('/api', require('./server/routes/api.route'));
+app.use('/auth', require('./routes/auth.route')); // POST /login  POST /register
+app.use('/api', require('./routes/api.route'));
 
-//serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    //set static folder
-    app.use(express.static('client/build'));
-    
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
-
-const port = process.env.PORT || 5000; //first component for HEROCU
+const port = 5000;
 
 app.listen(port, () => console.log(`server started on port ${port}`));
